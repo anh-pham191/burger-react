f72c8ee81ae92c39f0b35edbc0d185db013cc507
tuananhpham94 <tuananh191194@gmail.com>
2020-04-10 10:54:03 +1200

    add build control

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients) // transform object into array with only keys
         .map(igKey => {
-            return [...Array(props.ingredients[igKey])]
+            return [...Array(props.ingredients[igKey])] // get the array with length only to know the size of the elements
                 .map((_, i) => {
                     return <BurgerIngredient key={igKey + i} type={igKey} />
                 })
-        });
+        }).reduce((arr, el) => {
+            return arr.concat(el) //flatten the array to check if no ingredients, check reduce documentation
+        }, []);
 
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
